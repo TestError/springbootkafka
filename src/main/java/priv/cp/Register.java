bb4dd015ed9e0818f87b5475a84cf4ac6c84a576
peng.chen <peng1.chen@zqgame.com>
2018-07-11 17:55:05 +0800

    update

--- src/main/java/priv/cp/Register.java
+++ src/main/java/priv/cp/Register.java
@@ -2,10 +2,16 @@ package priv.cp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 import java.util.Date;
 
@@ -21,6 +27,9 @@ public class Register {
     public RouterFunction<ServerResponse> monoRouterFunction() {
         return route(POST("/echo"), serverRequest -> {
 
+//            ServerResponse serverResponse = ServerResponse.BodyBuilder
+
+//            return serverResponse;
             return ServerResponse.ok().body(fromObject(new Date().getTime()));
 
         });
--- src/test/java/priv/cp/AppTest.java
+++ src/test/java/priv/cp/AppTest.java
@@ -3,6 +3,8 @@ package priv.cp;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Unit test for simple App.
@@ -15,6 +17,12 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+
+//        var test = Mono.just()
+
+        Mono.just("qqq").subscribe(System.out::println);
+
+        Flux.just(1,2,3,4,5).subscribe(System.out::println);
+
     }
 }
