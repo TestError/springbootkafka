a0e5981f1492feb357a4e927c3ce852889e6b9b0
peng.chen <peng1.chen@zqgame.com>
2018-07-12 23:08:47 +0800

    1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,12 @@ module spring.boot.kafka {
     requires spring.webflux;
     requires reactor.core;
     requires spring.web;
+    requires spring.boot.starter.data.redis.reactive;
+
+    requires spring.beans;
+    requires spring.boot.starter.data.redis;
+    requires spring.data.redis;
+    requires spring.data.keyvalue;
 
     requires java.sql;
     requires slf4j.api;
--- src/main/java/priv/cp/Register.java
+++ src/main/java/priv/cp/Register.java
@@ -2,7 +2,9 @@ package priv.cp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -21,6 +23,10 @@ public class Register {
 
     private static Logger logger = LoggerFactory.getLogger(Register.class);
 
+    @Autowired
+    private ReactiveRedisTemplate reactiveRedisTemplate;
+//    private ReactiveHashOperations
+
     @Bean
     public RouterFunction<ServerResponse> monoRouterFunction() {
         return route(POST("/echo"), serverRequest -> {
@@ -33,6 +39,26 @@ public class Register {
 
             return ServerResponse.ok().body(Mono.just("1111"),String.class);
 
+        }).andRoute(POST("/testSet"),request -> {
+
+            var value = request.bodyToMono(String.class);
+
+//            Mono mono = value.su
+
+            value.subscribe(s -> {
+
+                logger.debug(s);
+                reactiveRedisTemplate.opsForValue().set("1",s).subscribe(o -> {logger.debug("oo:{}",o);});
+            });
+
+//            value.
+
+//            value.subscribe(s -> {
+
+//                reactiveRedisTemplate.opsForValue().getAndSet("1",s);
+
+//            });
+            return ServerResponse.ok().body(reactiveRedisTemplate.opsForValue().get("1"),String.class);
         });
     }
 
